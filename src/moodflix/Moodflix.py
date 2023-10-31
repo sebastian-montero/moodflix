c6619afffff690d4a2bf7a60a1d4967d747ec06f
Sebastian Montero <smonteroparis@icloud.com>
2023-10-31 20:15:40 +0000

    misc

--- src/moodflix/Moodflix.py
+++ src/moodflix/Moodflix.py
@@ -12,15 +12,13 @@ st.set_page_config(page_title="Moodflix", page_icon="🍿", layout="centered", m
     })
 
 st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
+"""
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+""",
     unsafe_allow_html=True
 )
 
